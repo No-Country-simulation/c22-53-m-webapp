94e6b3449a1a73ef8b6872511a3dc9fd8094e1c8
Tolomatico <ballesty.t@gmail.com>
2024-12-02 00:59:01 -0300

    Change medics route

--- backend/src/routes/medics.route.ts
+++ backend/src/routes/medics.route.ts
@@ -7,7 +7,7 @@ const router=Router()
 
 router.post("/auth/medic-register",register)
 router.post("/profile/files",authenticate,uploadFile)
-router.get("/medics/cv",getMedics)
+router.get("/medics",getMedics)
 export  {
     router as medicRouter
 }
