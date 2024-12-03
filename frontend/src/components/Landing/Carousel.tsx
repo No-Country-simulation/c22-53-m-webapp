6b7faf9da86123ae16d9bbe67cf1402a6f5ec75a
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-12-03 16:31:06 -0300

    deleted unused imports

--- frontend/src/components/Landing/Carousel.tsx
+++ frontend/src/components/Landing/Carousel.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import Slider from "react-slick";
-import { Box, Button } from "@mui/material";
+import { Box } from "@mui/material";
 
 export const Carousel = () => {
   const settings = {
