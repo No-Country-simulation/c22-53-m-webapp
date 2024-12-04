1a0245046eae69510b9abd48de35e02e1c30cc72
JoseB852 <jbetancourts976@gmail.com>
2024-12-04 13:16:25 -0300

    nabvar-user

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+export const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="bg-white py-2 fixed top-0 left-0 right-0 shadow-md z-50">
+      <div className="flex items-center justify-between px-4">
+        <button onClick={() => setOpen(!open)} className="md:hidden">
+          <i className="fa-solid fa-bars"></i>
+        </button>
+        <Link to="/">
+        <div>
+           <img
+            className="w-auto h-[50px] md:h-[80px]"
+            src="https://png.pngtree.com/png-clipart/20200727/original/pngtree-health-logo-design-logo-for-the-clinic-pharmaceutical-company-logo-of-png-image_5156715.jpg"
+            alt="Logo" />
+        </div>
+         
+        </Link>
+        <ul className="hidden md:flex space-x-6 ml-4">
+        <li className="hover:text-cyan-600">
+            <a href="/">Inicio</a>
+          </li>
+          <li className="hover:text-cyan-600">
+            <a href="#">Doctores y Centros</a>
+          </li>
+          <li className="hover:text-cyan-600">
+            <a href="#">Especialidades</a>
+          </li>
+          <li className="hover:text-cyan-600">
+            <a href="#">Enfermedades</a>
+          </li>
+        </ul>
+        <Link to="/login">
+          <button className="bg-cyan-600 text-white px-4 py-2 rounded-lg hover:bg-cyan-700 transition duration-300">
+            <i className="fa-regular fa-user"></i>
+          </button>
+        </Link>
+
+      </div>
+      <div
+        className={`${!open && "hidden"} bg-blue-900/50 min-h-screen w-full fixed top-0 left-0 backdrop-blur-sm z-40`}
+        onClick={() => setOpen(false)}></div>
+      <div className={`${open ? "w-64" : "w-0"} bg-white min-h-screen shadow-md fixed top-0 left-0 transition-all duration-300 overflow-hidden z-50`}>
+        <div className="pt-3">
+          <button
+            className="ml-4 text-black mb-6"
+            onClick={() => setOpen(false)}>
+            <i className="fa-solid fa-xmark text-black"></i>
+          </button>
+          <div className="text-center">
+            <ul>
+              <li className="text-black-400 text-m hover:bg-cyan-600 cursor-pointer py-2">
+                <a href="#">Como pedir una cita</a>
+              </li>
+              <li className="text-black-400 text-m hover:bg-cyan-600 cursor-pointer py-2">
+                <a href="#">Telemedicina</a>
+              </li>
+              <li className="text-black-400 text-m hover:bg-cyan-600 cursor-pointer py-2">
+                <a href="#">Doctores</a>
+              </li>
+              <li className="text-black-400 text-m hover:bg-cyan-600 cursor-pointer py-2">
+                <a href="#">Centros</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
